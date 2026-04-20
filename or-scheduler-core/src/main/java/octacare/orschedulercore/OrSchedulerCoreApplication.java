af32781761f19eeabfb210733d391ae0645d1fdd
Raul-Nicolae Constantin <constantin.raul10@gmail.com>
2026-04-20 17:15:51 +0300

    Add Spring Boot backend

---
+++ or-scheduler-core/src/main/java/octacare/orschedulercore/OrSchedulerCoreApplication.java
@@ -0,0 +1,13 @@
+package octacare.orschedulercore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrSchedulerCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrSchedulerCoreApplication.class, args);
+    }
+
+}
---
+++ or-scheduler-core/src/test/java/octacare/orschedulercore/OrSchedulerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package octacare.orschedulercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrSchedulerCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
