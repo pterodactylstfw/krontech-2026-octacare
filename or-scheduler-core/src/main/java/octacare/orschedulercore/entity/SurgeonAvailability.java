2d0a9f8215821b2ac46aad6c2f44d341bae0e55a
mdrian <florin.mincu@student.unitbv.ro>
2026-05-09 19:42:45 +0300

    feat: not null in surgeon availability database

--- or-scheduler-core/src/main/java/octacare/orschedulercore/entity/SurgeonAvailability.java
+++ or-scheduler-core/src/main/java/octacare/orschedulercore/entity/SurgeonAvailability.java
@@ -30,14 +30,14 @@ public class SurgeonAvailability {
     @Column(nullable = false)
     private LocalDate date;
 
-    @Column(name = "start_time")
+    @Column(name = "start_time", nullable = false)
     private LocalTime startTime;
 
-    @Column(name = "end_time")
+    @Column(name = "end_time", nullable = false)
     private LocalTime endTime;
 
-    @Column(name = "is_available")
-    private Boolean isAvailable;
+    @Column(name = "is_available", nullable = false)
+    private boolean isAvailable;
 
     @Enumerated(EnumType.STRING)
     private AvailabilityReason reason;
